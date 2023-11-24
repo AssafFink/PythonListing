ab8a061c71ed6bef8552ce241e5836af28c8fc9f
niv199 <niv1999@gmail.com>
2023-11-24 12:58:35 +0200

    Add printing 1 to 100

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ def print100to1():
     for i in range(100, 0, -1):
         print(i, end=" | ")
 print100to1()
+
+def print1to100():
+    for i in range (1, 101):
+        print(i, end=" | ")
+
+print1to100()
