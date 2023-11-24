61be3b1211072555614175e0e3b082bdae1d8164
AssafFink <assaf.fink@gmail.com>
2023-11-24 12:40:44 +0200

    print 100 to 1

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 
-def print1to100():
-    for i in range(1, 101):
-        print(i, end=" | ")
-
-print1to100()
-
-# def print100to1():
-#     for i in range(100, 0, -1):
+# def print1to100():
+#     for i in range(1, 101):
 #         print(i, end=" | ")
+# print1to100()
 
-# print100to1()
+def print100to1():
+    for i in range(100, 0, -1):
+        print(i, end=" | ")
+print100to1()
